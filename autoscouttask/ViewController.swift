aa67964b905f9fd630b1b7307e0b372fa0eec753
Kerem Uslular <kerem.uslular@vitecsoftware.com>
2023-02-28 13:57:58 +0300

    Initial Commit

---
+++ autoscouttask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoscouttask
+//
+//  Created by Kerem Uslular on 28.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
